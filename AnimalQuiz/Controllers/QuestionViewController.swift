d2827d123b1b9cd0c383de098fc2c9d44ca85200
Burdakovskiy <www.deluxesss@gmail.com>
2021-11-23 19:05:35 +0200

    added question and result view controllers

---
+++ AnimalQuiz/Controllers/QuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by Дмитрий on 23.11.2021.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ AnimalQuiz/Controllers/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  AnimalQuiz
+//
+//  Created by Дмитрий on 23.11.2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.hidesBackButton = true
+        
+    }
+    
+
+}
